08ed2a4c6a585cb930aedc2e183d93c35d871276
VitorHugoAntunes <vitor.passos3@etec.sp.gov.br>
2020-03-28 20:56:19 -0300

    Front-end application

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -48,7 +48,7 @@ export default function Register() {
                     <img src={logoImg} alt="Be The Hero"/>
 
                     <h1>Cadastro</h1>
-                    <p>Faça seu cadastro. Entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
                     <Link className="Back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041"/>
