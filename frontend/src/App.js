ae202f39a1d979bd0f80a222bbe2da60456c6e3d
VitorHugoAntunes <vitor.passos3@etec.sp.gov.br>
2020-03-28 20:50:02 -0300

    Back-end and Front-end application

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './global.css'
+
+import Routes from './routes';
+
+/**
+ * JSX = JavaScript XML (HTML integrado no JavaScript)
+ * Componentes: Funcao que retorna HTML, funcionalidades de JS e CSS
+ */
+
+function App() {
+
+  
+return (
+  <Routes/>
+  );
+}
+
+export default App;
